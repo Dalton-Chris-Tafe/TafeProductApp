8d096096056354cc125304f73a075a85a6d80e57
000973387 <Dalton.CHRISTOPHER@student.tafesa.edu.au>
2023-03-31 09:47:49 +1030

    Added the delivery charge $25

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -34,6 +34,9 @@ namespace ProductApps
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+
+                //Calc total charge after adding shipping cost
+                totalChargeTextBox.Text = (cProduct.TotalPayment + 25).ToString("C");
             }
             catch (FormatException)
             {
